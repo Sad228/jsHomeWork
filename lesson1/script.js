89fef0bfd85696c5af75db145ee99fbdf405cc43
Sad228 <mov4ko@gmail.com>
2023-02-01 10:33:43 +0200

    Lesson1

---
+++ lesson1/script.js
@@ -0,0 +1,63 @@
+let hello = 'hello';
+console.log(hello);
+
+let owu = 'owu';
+console.log(owu);
+
+let com = 'com';
+console.log(com);
+
+let ua = 'ua';
+console.log(ua)
+
+let num1 = 1;
+console.log(num1);
+
+let num2 = 10;
+console.log(num1);
+
+let num3 = -999;
+console.log(num3);
+
+let num4 = 123;
+console.log(num4);
+
+const PI = 3.14;
+console.log(PI);
+
+let num6 = 2.7;
+console.log(num6);
+
+let num7 = 16;
+console.log(num7);
+
+let tr = true;
+console.log(tr);
+
+
+let fls = false;
+console.log(fls);
+
+let a = 100;
+console.log(typeof a);
+
+let b = '100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c);
+
+let firstName = 'Pavlo';
+let lastName = 'Movchko ';
+let surName = ' Igorovich';
+let person = (lastName + firstName + surName);
+console.log(person);
+
+name = prompt('Your Name');
+console.log(name);
+
+sur = prompt('Sur Name');
+console.log(sur);
+
+age = prompt('age');
+console.log(age);
