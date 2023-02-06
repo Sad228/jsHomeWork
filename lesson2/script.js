e5ba4708bbf3376511716a367b6d8e9e1c6b8072
Sad228 <mov4ko@gmail.com>
2023-02-06 14:16:28 +0200

    lesson2

--- lesson2/script.js
+++ lesson2/script.js
@@ -49,13 +49,6 @@ users3 = [
     name: 'ostap',
     username: 'patso',
     password: 'passworsOstap',
-    // age: 12,
-    // number: 12345,
-    //  email: 'ostap@gmail.com',
-    // height: 175,
-    // weight: 75,
-    // job: 'driver',
-    // country:'Poland',
     },
     {
         name: 'petro',
@@ -154,5 +147,34 @@ console.log(users3[2].password);
 // }
 
 
+let a = 'Lorem ipsum';
+b = a || 'default';
+console.log(b);
 
 
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+if (coursesAndDurationArray[0].monthDuration > 5) {
+    console.log('Super');
+}
+if (coursesAndDurationArray[1].monthDuration > 5) {
+    console.log('Super');
+}
+if (coursesAndDurationArray[2].monthDuration > 5) {
+    console.log('Super');
+}
+if (coursesAndDurationArray[3].monthDuration > 5) {
+    console.log('Super');
+}
+if (coursesAndDurationArray[4].monthDuration > 5) {
+    console.log('Super');
+}
+if (coursesAndDurationArray[5].monthDuration > 5) {
+    console.log('Super');
+}
